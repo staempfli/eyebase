c8a1840fe3a1b7d081550e1febb8aaf26ed2321a
Marcel Hauri <marcel@hauri.me>
2017-08-03 21:47:06 +0200

    refactor eyebase basic methods into abstract and add basic api methods in api

--- src/Staempfli/Eyebase/Api.php
+++ src/Staempfli/Eyebase/Api.php
@@ -4,168 +4,100 @@ namespace Staempfli\Eyebase;
 
 use GuzzleHttp\Client;
 
-class Api
+class Api extends Eyebase
 {
-    const DEFAULT_API_VERSION = 1;
-
     /**
      * @var Client
      */
     private $client;
-    /**
-     * @var int
-     */
-    private $version = self::DEFAULT_API_VERSION;
-    /**
-     * @var string
-     */
-    private $url = '';
-    /**
-     * @var string
-     */
-    private $username = '';
-    /**
-     * @var string
-     */
-    private $password = '';
-    /**
-     * @var string
-     */
-    private $token = '';
-    /**
-     * @var array
-     */
-    private $defaultParams = [
-        'fx' => 'api',
-        'qt' => 'r'
-    ];
 
     /**
      * Api constructor.
      * @param string $url
-     * @param int $version
+     * @param string $token
      */
-    public function __construct(string $url = '', int $version = self::DEFAULT_API_VERSION)
+    public function __construct(string $url = '', string $token = '')
     {
         $this->client = new Client();
         $this->setUrl($url);
-        $this->setVersion($version);
-    }
-
-    /**
-     * @return int
-     */
-    public function getVersion()
-    {
-        return $this->version;
-    }
-
-    /**
-     * @param int $version
-     * @return $this
-     */
-    public function setVersion(int $version)
-    {
-        $this->version = $version;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
-    }
-
-    /**
-     * @param string $url
-     * @return $this
-     */
-    public function setUrl(string $url)
-    {
-        $this->url = $url;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @param string $username
-     * @return $this
-     */
-    public function setUsername(string $username)
-    {
-        $this->username = $username;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param string $password
-     * @return $this
-     */
-    public function setPassword(string $password)
-    {
-        $this->password = $password;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getToken()
-    {
-        return $this->token;
-    }
-
-    /**
-     * @param string $token
-     * @return $this
-     */
-    public function setToken(string $token)
-    {
-        $this->token = $token;
-        return $this;
+        $this->setToken($token);
     }
 
     /**
      * @param array $params
+     * @return string
      */
-    public function request(array $params =[])
+    public function request(array $params = [])
     {
-        $url = sprintf('%s/api/%d/webmill.php?%s',
+        $parameters = array_merge($this->getDefaultParams(), $params);
+        $url = sprintf(
+            '%s/api/%d/webmill.php?%s',
             rtrim($this->getUrl(), '/'),
             $this->getVersion(),
-            array_merge($this->getDefaultParams(), http_build_query($params))
+            http_build_query($parameters)
         );
 
-        $response = $this->client->get($url)->getBody();
-        $test = 0;
+        $response = $this->client->get($url);
+        $content = $response->getBody()->getContents();
+        return $this->convertXmlToJson($content);
     }
 
     /**
-     * @return array
+     * @param string $xml
+     * @return string
      */
-    private function getDefaultParams()
+    private function convertXmlToJson(string $xml)
     {
-        $params = $this->defaultParams;
-        $params['benutzer'] = $this->getUsername();
-        $params['ben_kennung'] = $this->getPassword();
-        $params['token'] = $this->getToken();
-        return $params;
+        $simpleXMLElement = simplexml_load_string($xml, null, LIBXML_NOCDATA);
+        return json_encode($simpleXMLElement);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiVersion()
+    {
+        return $this->request(['qt' => 'version']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLoginStatus()
+    {
+        return $this->request(['qt' => 'loginstatus']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getFolderTree()
+    {
+        return $this->request(['qt' => 'ftree']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvailableLanguages()
+    {
+        return $this->request(['qt' => 'lang']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvailableMediaAssetTypes()
+    {
+        return $this->request(['qt' => 'mat']);
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function fullTextSearch(string $text)
+    {
+        return $this->request(['ftx' => $text]);
     }
 }
---
+++ src/Staempfli/Eyebase/Eyebase.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Staempfli\Eyebase;
+
+abstract class Eyebase
+{
+    const DEFAULT_API_VERSION = 1;
+
+    /**
+     * @var int
+     */
+    private $version = self::DEFAULT_API_VERSION;
+    /**
+     * @var string
+     */
+    private $url = '';
+    /**
+     * @var string
+     */
+    private $username = '';
+    /**
+     * @var string
+     */
+    private $password = '';
+    /**
+     * @var string
+     */
+    private $token = '';
+    /**
+     * @var array
+     */
+    private $defaultParams = [
+        'fx' => 'api',
+        'qt' => 'r'
+    ];
+
+    /**
+     * @return int
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param int $version
+     * @return $this
+     */
+    public function setVersion(int $version)
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    public function setUrl(string $url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return $this
+     */
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     * @return $this
+     */
+    public function setToken(string $token)
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getDefaultParams()
+    {
+        $params = $this->defaultParams;
+        $params['benutzer'] = $this->getUsername();
+        $params['ben_kennung'] = $this->getPassword();
+        $params['token'] = $this->getToken();
+        return array_filter($params);
+    }
+}
