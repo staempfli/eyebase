636ebc01530f96b66b4b912d12b2f436985e08b5
Marcel Hauri <marcel@hauri.me>
2017-08-08 14:24:50 +0200

    extend tests, add test server

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -8,7 +8,7 @@ class ApiTest extends PHPUnit_Framework_TestCase
 
     public function __construct()
     {
-        $this->api = new Api();
+        $this->api = new Api('http://localhost:8082', 'TEST_TOKEN');
         parent::__construct();
     }
 
@@ -20,5 +20,43 @@ class ApiTest extends PHPUnit_Framework_TestCase
     public function testDefaultOutputFormat()
     {
         $this->assertSame('xml', $this->api->getOutputFormat());
+    }
+
+    public function testReturnResultAsSimpleXMLElement()
+    {
+        $result = $this->api->getApiVersion();
+        $this->assertInstanceOf(SimpleXMLElement::class, $result);
+    }
+
+    public function testReturnResultAsArray()
+    {
+        $result = $this->api->setOutputFormat('array')->getApiVersion();
+        $this->assertTrue(is_array($result));
+    }
+
+    public function testReturnResultAsJson()
+    {
+        $string = '{"version":{"id":"1.0.0","name":"eyebase TEST API v1.0.0"}}';
+        $result = $this->api->setOutputFormat('json')->getApiVersion();
+        $this->assertTrue(is_string($result));
+        $this->assertSame($string, $result);
+    }
+
+    public function testLoginStatus()
+    {
+        $result = $this->api->getLoginStatus();
+        $this->assertSame('User logged out.', (string) $result->message->text);
+    }
+
+    public function testLoginSuccess()
+    {
+        $result = $this->api->login('api', 'api');
+        $this->assertSame('Login successful', (string) $result->user->message);
+    }
+
+    public function testLoginFailed()
+    {
+        $result = $this->api->login('api', 'test');
+        $this->assertSame('Login failed', (string) $result->user->message);
     }
 }
